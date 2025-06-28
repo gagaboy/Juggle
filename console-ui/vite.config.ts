9b5d5ee7feb18fbd0ec240f46f82d804cc998e59
Eric Chang <gagaboy@126.com>
2025-06-28 23:38:37 +0800

    配置文件修改

--- console-ui/vite.config.ts
+++ console-ui/vite.config.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: Eric Chang gagaboy@126.com
+ * @Date: 2025-02-21 16:23:39
+ * @LastEditors: Eric Chang gagaboy@126.com
+ * @LastEditTime: 2025-03-07 18:27:30
+ * @FilePath: /Juggle/console-ui/vite.config.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -6,6 +14,7 @@ import monacoEditorPlugin from 'vite-plugin-monaco-editor'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
+  console.log("后端 API 代理地址："+env.VITE_API_PROXY)
   return {
     plugins: [
       vue(),
